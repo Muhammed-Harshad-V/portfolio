acea66e5f745d4abf2584cd2c4bb851a7b224330
Meagamer <irshadiru472@gmail.com>
2024-12-31 15:48:58 +0530

    some expirements with colors

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="w-full h-[60px] bg-repeat bg-[url('/navbg.png')] border-b-[3px] border-White "></div>
+
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        White: "#F9F9F9",
       },
     },
   },
