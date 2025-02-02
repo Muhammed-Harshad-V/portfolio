0bed3100a864a992d37d27f466419ed9f9469d3d
Muhammed-Harshad-V <irshadiru472@gmail.com>
2025-02-02 20:01:58 +0530

    minor changes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -44,89 +44,94 @@ const Home = () => {
 
           {/* {what i use} */}
 
-          <div className="w-full bg-black rounded-t-3xl border-t-2 border-t-retroWhite font-mono sm:pr-5 lg:pr-8 xl:pr-10 sm:pl-5 lg:pl-8 xl:pl-10">
-              <h2 className='text-retroOrange sm:text-[20px] lg:text-[25px] font-bold sm:ml-[30px] sm:mt-[30px] md:ml-[40px] lg:ml-[60px] lg:mt-[40px] xl:mt-[50px] mb-[40px]'>
-                What I Use!
-              </h2>
-              <div className="grid sm:grid-cols-1 xl:grid-cols-3 border-2 rounded-xl mb-[100px] bg-black drop-shadow-whiteGlow">
-                <div className="sm:h-[250px] md:h-[300px] lg:h-[380px] xl:h-[450px] relative flex justify-center items-center">
-                  <Image
-                    src="/test.png"
-                    alt="Description"
-                    layout="responsive" // Adjust the layout to responsive
-                    width={700} // Original width of the image
-                    height={400} // Original height of the image
-                    className="max-w-[60%] md:max-w-[45%] lg:max-w-[35%] xl:max-w-[70%] h-auto" // Set max-width to 70% of the container
-                  />
+                    <div className="w-full bg-black rounded-t-3xl border-t-2 border-t-retroWhite font-mono sm:pr-5 lg:pr-8 xl:pr-10 sm:pl-5 lg:pl-8 xl:pl-10">
+                <h2 className="text-retroOrange sm:text-[20px] lg:text-[25px] font-bold sm:ml-[30px] sm:mt-[30px] md:ml-[40px] lg:ml-[60px] lg:mt-[40px] xl:mt-[50px] mb-[40px]">
+                  My Tech Stack
+                </h2>
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 border-2 rounded-xl mb-[100px] bg-black drop-shadow-whiteGlow p-6">
+                  {/* MERN Stack */}
+                  <div className="flex justify-center items-center">
+                    <Image
+                      src="/express-js.png"
+                      alt="Express"
+                      width={100}
+                      height={100}
+                      className="object-contain"
+                    />
+                  </div>
+
+                  {/* Next.js */}
+                  <div className="flex justify-center items-center">
+                    <Image
+                      src="/nextjs.webp"
+                      alt="Next.js"
+                      width={100}
+                      height={100}
+                      className="object-contain"
+                    />
+                  </div>
+                  {/* Node.js */}
+                  <div className="flex justify-center items-center">
+                    <Image
+                      src="/node-js-.png"
+                      alt="Node.js"
+                      width={100}
+                      height={100}
+                      className="object-contain"
+                    />
+                  </div>
+                  {/* MongoDB */}
+                  <div className="flex justify-center items-center">
+                    <Image
+                      src="/leaf.png"
+                      alt="MongoDB"
+                      width={100}
+                      height={100}
+                      className="object-contain"
+                    />
+                  </div>
+                  {/* Bun */}
+                  <div className="flex justify-center items-center">
+                    <Image
+                      src="/docker-logo-blue.png"
+                      alt="Docker"
+                      width={100}
+                      height={100}
+                      className="object-contain"
+                    />
+                  </div>
+                  {/* TypeScript */}
+                  <div className="flex justify-center items-center">
+                    <Image
+                      src="/ts.png"
+                      alt="TypeScript"
+                      width={100}
+                      height={100}
+                      className="object-contain"
+                    />
+                  </div>
+
+                  <div className="flex justify-center items-center">
+                    <Image
+                      src="/react-js-.png"
+                      alt="react"
+                      width={100}
+                      height={100}
+                      className="object-contain"
+                    />
+                  </div>
+
+                  <div className="flex justify-center items-center">
+                    <Image
+                      src="/th.jpeg"
+                      alt="tailwind"
+                      width={100}
+                      height={100}
+                      className="object-contain"
+                    />
+                  </div>
                 </div>
 
-                <div className="lg:col-span-2 pb-10 sm:pr-3 md:pr-5 lg:pr-8 xl:pr-10 lg:pt-10">
-                  <p className='text-retroWhite sm:text-[13px] lg:text-[18px] sm:ml-[30px] sm:mt-[10px] md:ml-[40px] lg:ml-[60px] lg:mt-[15px] xl:mt-[20px] leading-relaxed'>
-                    At the heart of everything I build is <span className="text-retroOrange font-bold">JavaScript</span> a versatile language that powers both the frontend and backend of my projects. Using the <span className="text-retroOrange font-bold">MERN Stack</span>, I create efficient, scalable, and user-friendly applications.
-                  </p>
-
-                  <p className='text-retroWhite sm:text-[13px] lg:text-[18px] sm:ml-[30px] sm:mt-[10px] md:ml-[40px] lg:ml-[60px] lg:mt-[10px] xl:mt-[15px] leading-relaxed'>
-                    From crafting seamless user interfaces with React to developing robust backends with Node.js and Express, and managing data effortlessly with MongoDB, I bring ideas to life with clean, modern, and impactful solutions.
-                  </p>
-
-                  <p className='text-retroWhite sm:text-[13px] lg:text-[18px] sm:ml-[30px] sm:mt-[10px] md:ml-[40px] lg:ml-[60px] lg:mt-[10px] xl:mt-[15px] leading-relaxed'>
-                    also next.js bun etc..
-                  </p>
-                </div>
-              </div>
-                  {/* My Journey Section */}
-                <div className="grid flex-col">
-                      <h2 className='text-retroOrange sm:text-[20px] lg:text-[25px] font-bold sm:ml-[30px] sm:mt-[30px] md:ml-[40px] lg:ml-[60px] lg:mt-[40px] xl:mt-[50px] mb-[40px]'>
-                        My Journey!
-                      </h2>
-
-                      {/* Oldest Project */}
-                      <div className="relative flex flex-col items-start pt-5 pl-5 pr-5 rounded-xl border-2 border-retroWhite drop-shadow-whiteGlow bg-black">
-                        <h3 className="text-retroOrange font-bold text-lg mb-2">Oldest Project</h3>
-                        <p className="text-retroWhite text-sm mb-2">
-                        This project was my first full endeavor in coding, where I cloned a website template using HTML and CSS. It was a significant milestone that deepened my understanding of web layout and styling. Although I successfully replicated the design, I faced challenges with responsiveness, which remains incomplete. This project taught me valuable lessons about web development and sparked my passion for creating intuitive user experiences.
-                        </p>
-                        <div className="text-white text-sm mb-1">
-                        <strong>Technologies Used:</strong> <span className='text-sky-400'>Html, css</span>
-                        </div>
-                        <div className="text-white text-sm mb-5">
-                          <strong>Status:</strong> <span className='text-red-600'>Not completed</span>
-                        </div>
-                        <div className="flex space-x-4 mb-8">
-                          <a href="https://github.com/yourusername/your-repo" target="_blank" rel="noopener noreferrer">
-                            <Image src="/githubIcon.png" alt="GitHub Repository" width={25} height={25} className="rounded-full" />
-                          </a>
-                          <a href="https://yourwebsite.com" target="_blank" rel="noopener noreferrer">
-                            <Image src="/globe.jpeg" alt="Live Website" width={25} height={25} className="rounded-full" />
-                          </a>
-                        </div>
-                      </div>
-
-                      {/* Connecting Line */}
-                      <div className="w-1 bg-retroWhite ml-10 sm:h-[60px] md:h-[80px] lg:[110px] xl:h-[150px] drop-shadow-purpleGlow"></div> 
-
-                      {/* Newest Project */}
-                      <div className="relative flex flex-col items-start pt-5 pr-5 pl-5 rounded-xl border-2 border-retroWhite mb-[100px] drop-shadow-whiteGlow bg-black">
-                        <h3 className="text-retroOrange font-bold text-lg mb-2">Newest Project</h3>
-                        <p className="text-retroWhite text-sm mb-2">
-                          A brief description of your newest project goes here. Highlight the features, technologies used, and what you learned from it.
-                        </p>
-                        <div className="text-white text-sm mb-1">
-                          <strong>Technologies Used:</strong> <span className='text-sky-400'>React, Node.js, MongoDB</span>
-                        </div>
-                        <div className="text-white text-sm mb-5">
-                          <strong>Status:</strong> <span className='text-retroGreen'>Completed</span>
-                        </div>
-                        <div className="flex space-x-4 mb-8">
-                          <a href="https://github.com/yourusername/your-repo" target="_blank" rel="noopener noreferrer">
-                            <Image src="/githubIcon.png" alt="GitHub Repository" width={25} height={25} className="rounded-full" />
-                          </a>
-                          <a href="https://yourwebsite.com" target="_blank" rel="noopener noreferrer">
-                            <Image src="/globe.jpeg" alt="Live Website" width={25} height={25} className="rounded-full" />
-                          </a>
-                        </div>
-                      </div>
-              </div>
               <div className='flex flex-col items-center justify-center mb-4 mt-4 text-sm'>
                 <Link href="/projects">
                   <p className="text-white mb-3"><span>[</span><span className='hover:text-retroOrange'>All Projects</span><span>]</span></p>
